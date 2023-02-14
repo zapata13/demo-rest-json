629c50b58c06f6bdd56b9db802c3f11ba4279a91
Eduardo Rivas Zapata <zapata@redhat.com>
2023-02-13 21:28:08 -0600

    Proyecto basico de quarkus en vscode

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{name}")
+    public String hello(String name){
+        return "Hello "+ name +", from RESTEasy Reactive";
+    }
+}
